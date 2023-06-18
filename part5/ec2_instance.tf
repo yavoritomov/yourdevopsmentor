c0a74211ec5f49a384190461c59e99a09c6bffc0
Yavor Tomov <yavoritomov@gmail.com>
2023-06-18 15:55:09 +0300

    change to amazon linux AMI adding private ec2

--- part5/ec2_instance.tf
+++ part5/ec2_instance.tf
@@ -23,6 +23,32 @@ resource "aws_security_group" "public" {
   }
 }
 
+resource "aws_security_group" "private" {
+  name        = "${var.env_code}-private"
+  description = "Private security group"
+  vpc_id      = aws_vpc.taxi_vpc.id
+
+  ingress {
+    description      = "SSH from VPC only"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = [var.vpc_cidr]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.env_code}-private"
+  }
+}
+
+
 resource "aws_key_pair" "tf-key-pair" {
 key_name = "tf-key-pair"
 public_key = tls_private_key.rsa.public_key_openssh
@@ -37,7 +63,7 @@ filename = "tf-key-pair"
 }
 
 resource "aws_instance" "public" {
-  ami           = "ami-06633e38eb0915f51" # us-west-2
+  ami           = "ami-0e820afa569e84cc1" # us-east-2 Amazon Linux
   instance_type = "t3.micro"
   associate_public_ip_address = true
   key_name = "tf-key-pair"
@@ -48,5 +74,19 @@ resource "aws_instance" "public" {
 
   tags = {
     Name = "${var.env_code}-public"
+  }
+}
+
+resource "aws_instance" "private" {
+  ami           = "ami-0e820afa569e84cc1" # us-east-2 Amazon Linux
+  instance_type = "t3.micro"
+  key_name = "tf-key-pair"
+  vpc_security_group_ids = [aws_security_group.private.id]
+  subnet_id = aws_subnet.private[0].id
+
+  depends_on = [local_file.tf-key]
+
+  tags = {
+    Name = "${var.env_code}-private"
   }
 }
--- part5/output.tf
+++ part5/output.tf
@@ -1,3 +1,7 @@
-output "ec2_ip" {
+output "ec2_ip_public" {
   value = aws_instance.public.*.public_ip
+}
+
+output "ec2_ip_private" {
+  value = aws_instance.private.*.private_ip
 }
